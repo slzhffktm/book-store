4b4e7532d96c88b2b543b6e1c35d4af08a4bcc23
Nicholas Wijaya <pinkappleberry98@gmail.com>
2018-11-29 15:46:18 +0700

    edit detail page

--- Pro-Book/frontend/book/detail/index.php
+++ Pro-Book/frontend/book/detail/index.php
@@ -22,7 +22,13 @@
                     <?php echo "<h1 style=\"color: orangered\">" . $result['Title'] . "</h1>" ?>
                     <?php echo "<h5>" . $result['Author'] . "</h5>" ?>
                     <br>
-                    <?php echo "<h3 style=\"color: orangered\"> Rp" . $result['Price'] . "</h3>" ?>
+                    <?php 
+                        if ($result['Price'] > 0) {
+                            echo "<h3 style=\"color: orangered\"> Rp" . $result['Price'] . "</h3>";
+                        } else {
+                            echo "<h3 style=\"color: orangered\">Not For Sale</h3>";
+                        }
+                    ?>
                     <?php echo "<p>" . $result['Description'] . "</p>" ?>
                 </div>
                 <div class="kolom-md-2">
@@ -68,7 +74,7 @@
                     <div id="order-btn-div" class="row" style="margin: auto">
                         <?php
                             if ($result["Price"] < 0){
-                                echo "<button disabled id=\"order-btn\">Not For Sale</button>";
+                                echo "<button disabled id=\"disabled-btn\">Order</button>";
                             }
                             else{
                                 echo "<button onclick=\"makeOrder('" . $result['ID'] . "')\" id=\"order-btn\">Order</button>";
